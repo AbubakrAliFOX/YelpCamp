d6dcdc2fd31ef091cda0c78318b0edae9a865343
Abubakr <thefox5000@gmail.com>
2023-10-21 12:50:38 +0300

    Add nice cluster map

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,73 +1,98 @@
-const Campground = require('../models/campgrounds');
-const {cloudinary} = require('../cloudinary');
-const axios = require('axios');
+const Campground = require("../models/campgrounds");
+const { cloudinary } = require("../cloudinary");
+const axios = require("axios");
 
-module.exports.index = async(req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', {campgrounds})
-}
+module.exports.index = async (req, res) => {
+  const campgrounds = await Campground.find({});
+  const geoData = {
+    type: "FeatureCollection",
+    features: [],
+  };
+  for (const campground of campgrounds) {
+    geoData.features.push({
+      type: "Feature",
+      properties: {},
+      geometry: campground.geometry,
+      title: campground.title,
+      id: campground._id
+    });
+  }
+  const campgrondsGeoJSON = JSON.stringify(geoData);
+  res.render("campgrounds/index", { campgrounds, campgrondsGeoJSON });
+};
 
 module.exports.renderNewForm = (req, res) => {
-    res.render('campgrounds/new');
-}
+  res.render("campgrounds/new");
+};
 
 module.exports.createCampground = async (req, res, next) => {
-    const campground = new Campground(req.body.campground);
-    campground.images = req.files.map(f => ({url: f.path, filename: f.filename}));
-    campground.author = req.user._id;
-    const geocode = await axios.get(`https://nominatim.openstreetmap.org/search?q=${campground.location}&format=geojson`);
-    campground.geometry = geocode.data.features[0].geometry;
-    console.log(campground);
-    await campground.save();
-    req.flash('success','Successfully made a new campground!');
-    res.redirect(`/campgrounds/${campground._id}`);
-}
+  const campground = new Campground(req.body.campground);
+  campground.images = req.files.map((f) => ({
+    url: f.path,
+    filename: f.filename,
+  }));
+  campground.author = req.user._id;
+  const geocode = await axios.get(
+    `https://nominatim.openstreetmap.org/search?q=${campground.location}&format=geojson`
+  );
+  campground.geometry = geocode.data.features[0].geometry;
+  console.log(campground);
+  await campground.save();
+  req.flash("success", "Successfully made a new campground!");
+  res.redirect(`/campgrounds/${campground._id}`);
+};
 
 module.exports.showCampground = async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate({
-        path: 'reviews',
-        populate: {
-            path: 'author'
-        }
-    }).populate('author');
-    if (!campground) {
-        req.flash('error', 'Cannot find that campground!');
-        return res.redirect('/campgrounds');
-    }
-    res.render('campgrounds/show', {campground});
-}
+  const campground = await Campground.findById(req.params.id)
+    .populate({
+      path: "reviews",
+      populate: {
+        path: "author",
+      },
+    })
+    .populate("author");
+  if (!campground) {
+    req.flash("error", "Cannot find that campground!");
+    return res.redirect("/campgrounds");
+  }
+  res.render("campgrounds/show", { campground });
+};
 
 module.exports.renderEditForm = async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    if (!campground) {
-        req.flash('error', 'Cannot find that campground!');
-        return res.redirect('/campgrounds');
-    }
-    res.render('campgrounds/edit', { campground });
-}
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  if (!campground) {
+    req.flash("error", "Cannot find that campground!");
+    return res.redirect("/campgrounds");
+  }
+  res.render("campgrounds/edit", { campground });
+};
 
 module.exports.updateCampground = async (req, res) => {
-    const {id} = req.params;
-    console.log(req.body);
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
-    const imgs = req.files.map(f => ({url: f.path, filename: f.filename}));
-    campground.images.push(...imgs);
-    if (req.body.deleteImages) {
-        for (let filename of req.body.deleteImages) {
-            await cloudinary.uploader.destroy(filename);
-        }
-        await campground.updateOne({$pull: {images: {filename: {$in: req.body.deleteImages}}}})
-        console.log(campground);
+  const { id } = req.params;
+  console.log(req.body);
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
+  const imgs = req.files.map((f) => ({ url: f.path, filename: f.filename }));
+  campground.images.push(...imgs);
+  if (req.body.deleteImages) {
+    for (let filename of req.body.deleteImages) {
+      await cloudinary.uploader.destroy(filename);
     }
-    await campground.save();
-    req.flash('success', 'Successfully updated!');
-    res.redirect(`/campgrounds/${campground._id}`);
-}
+    await campground.updateOne({
+      $pull: { images: { filename: { $in: req.body.deleteImages } } },
+    });
+    console.log(campground);
+  }
+  await campground.save();
+  req.flash("success", "Successfully updated!");
+  res.redirect(`/campgrounds/${campground._id}`);
+};
 
-module.exports.deleteCampground = async(req, res) => {
-    const {id} = req.params;
-    await Campground.findByIdAndDelete(id);
-    req.flash('success', 'Successfully deleted campground!')
-    res.redirect('/campgrounds');
-}
+module.exports.deleteCampground = async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  req.flash("success", "Successfully deleted campground!");
+  res.redirect("/campgrounds");
+};
--- seeds/index.js
+++ seeds/index.js
@@ -1,51 +1,58 @@
-const mongoose = require('mongoose');
-const Campground = require('../models/campgrounds');
-const cities = require('./cities');
-const {places, descriptors} = require('./seedHelpers');
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+const mongoose = require("mongoose");
+const Campground = require("../models/campgrounds");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-    console.log('Database Connected');
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database Connected");
 });
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
-const seedDB = async() => {
-    await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const randPrice = Math.floor(Math.random() * 20) + 10;
-        const camp = new Campground({
-            author: '6518ebfba7b58a06c3f30a46',
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem unde perspiciatis doloribus impedit, consectetur eligendi dignissimos dolores. Adipisci natus voluptas voluptate nam cumque perferendis dolorum praesentium, voluptatem perspiciatis, earum suscipit',
-            price: randPrice,
-            images: [
-                {
-                  url: 'https://res.cloudinary.com/dewpck9nt/image/upload/v1696244107/YelpCamp/ociocxznbh2gdlpgrtia.jpg',
-                  filename: 'YelpCamp/ociocxznbh2gdlpgrtia'
-                },
-                {
-                  url: 'https://res.cloudinary.com/dewpck9nt/image/upload/v1696244109/YelpCamp/o8edi1mtlpccdmexjwsn.jpg',
-                  filename: 'YelpCamp/o8edi1mtlpccdmexjwsn'
-                },
-                {
-                  url: 'https://res.cloudinary.com/dewpck9nt/image/upload/v1696244111/YelpCamp/uwnusro8pfpwjfjmhyrp.jpg',
-                  filename: 'YelpCamp/uwnusro8pfpwjfjmhyrp'
-                }
-              ]
-        })
-        await camp.save();
-    }
-}
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 200; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const randPrice = Math.floor(Math.random() * 20) + 10;
+    const camp = new Campground({
+      author: "6518ebfba7b58a06c3f30a46",
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem unde perspiciatis doloribus impedit, consectetur eligendi dignissimos dolores. Adipisci natus voluptas voluptate nam cumque perferendis dolorum praesentium, voluptatem perspiciatis, earum suscipit",
+      price: randPrice,
+      geometry: {
+        type: "Point",
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ]
+      },
+      images: [
+        {
+          url: "https://res.cloudinary.com/dewpck9nt/image/upload/v1696244107/YelpCamp/ociocxznbh2gdlpgrtia.jpg",
+          filename: "YelpCamp/ociocxznbh2gdlpgrtia",
+        },
+        {
+          url: "https://res.cloudinary.com/dewpck9nt/image/upload/v1696244109/YelpCamp/o8edi1mtlpccdmexjwsn.jpg",
+          filename: "YelpCamp/o8edi1mtlpccdmexjwsn",
+        },
+        {
+          url: "https://res.cloudinary.com/dewpck9nt/image/upload/v1696244111/YelpCamp/uwnusro8pfpwjfjmhyrp.jpg",
+          filename: "YelpCamp/uwnusro8pfpwjfjmhyrp",
+        },
+      ],
+    });
+    await camp.save();
+  }
+};
 
 seedDB().then(() => {
-    mongoose.connection.close();
-})
-
+  mongoose.connection.close();
+});
